f8f8872e2ffda99f9b1bb55228599b767ff7f8ca
OVI <OVI@DESKTOP-DF5TG5J>
2018-12-17 23:19:06 +0900

    Add project files.

---
+++ DemoMongo/DemoSet1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace DemoMongo
+{
+   public class DemoSet1
+    {
+         //List data
+        public static void main()
+        {
+            MongoClient client = new MongoClient();
+
+            #region List Data
+            var db_list = client.ListDatabases().ToList();
+            foreach (var db in db_list)
+            {
+                Console.WriteLine(db.ToJson());
+            }
+            #endregion
+
+
+            Console.ReadKey();
+
+        }
+         
+
+    }
+}
---
+++ DemoMongo/Program.cs
@@ -0,0 +1,16 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoMongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            DemoSet1.main();
+        }
+    }
+}
