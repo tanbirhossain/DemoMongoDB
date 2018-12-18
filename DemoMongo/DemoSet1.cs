31595c4d5142da7480a26270049166626d7a2166
OVI <OVI@DESKTOP-DF5TG5J>
2018-12-18 22:25:17 +0900

    Crete New Collection

--- DemoMongo/DemoSet1.cs
+++ DemoMongo/DemoSet1.cs
@@ -15,14 +15,32 @@ namespace DemoMongo
         {
             MongoClient client = new MongoClient();
 
-            #region List Data
+            Console.WriteLine("------  Start database hit-------");
+            #region List DataBase
             var db_list = client.ListDatabases().ToList();
+            
             foreach (var db in db_list)
             {
+                
                 Console.WriteLine(db.ToJson());
             }
             #endregion
 
+            #region Showing List Collect
+            //var collection = client.GetDatabase("demoDb").GetCollection<BsonDocument>("products");
+            //var docList = collection.Find(new BsonDocument()).ToList();
+
+            //foreach (var item in docList)
+            //{
+            //    Console.WriteLine(item.ToJson()); 
+
+            //}
+            #endregion
+
+
+            #region Crete New Collection
+            client.GetDatabase("csharp_demo_db").CreateCollection("products");
+            #endregion
 
             Console.ReadKey();
 
