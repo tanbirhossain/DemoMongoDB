fbf175c90073a5187863f19bab5e6da0150843d5
OVI <OVI@DESKTOP-DF5TG5J>
2019-01-15 22:48:10 +0900

    update

--- DemoMongo/FindDemo.cs
+++ DemoMongo/FindDemo.cs
@@ -24,15 +24,52 @@ namespace DemoMongo
             #endregion
 
             #region Find With query Collection
+            //BsonDocument doc = new BsonDocument();
+            //doc.Add("price", 180); //this is condition
+            //List<BsonDocument> list_docs = collection.Find(doc).ToList();
+            //foreach (var item in list_docs)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region Limit show collection
+            //BsonDocument doc = new BsonDocument();
+            ////doc.Add("price", 180);
+            //List<BsonDocument> list_docs = collection.Find(doc).Limit(3).ToList();
+            //foreach (var item in list_docs)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Sorting
+            // BsonDocument doc = new BsonDocument();
+            // var builder = Builders<BsonDocument>.Sort;
+            // var sorting = builder.Ascending("price");
+
+           
+            // List<BsonDocument> list_docs = collection.Find(doc).Sort(sorting).Limit(3).ToList();
+            // foreach (var item in list_docs)
+            // {
+            //     Console.WriteLine(item);
+            // }
+            #endregion
+
+            #region Projection
             BsonDocument doc = new BsonDocument();
-            doc.Add("price", 180);
-            List<BsonDocument> list_docs = collection.Find(doc).ToList();
+            var builder_proj = Builders<BsonDocument>.Projection;
+            var projection_config = builder_proj.Include("price").Include("name").Exclude("_id");
+            
+            List<BsonDocument> list_docs = collection.Find(doc).Project(projection_config).Limit(4).ToList();
             foreach (var item in list_docs)
             {
                 Console.WriteLine(item);
             }
-
             #endregion
+
+
 
             Console.ReadKey();
         }
