8c2723b77b4c9110c8640fac66835b2bbd2cb860
OVI <OVI@DESKTOP-DF5TG5J>
2019-01-15 22:25:13 +0900

    find with query

---
+++ DemoMongo/FindDemo.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoMongo
+{
+    public class FindDemo
+    {
+        public static void main()
+        {
+            MongoClient client = new MongoClient();
+            var collection = client.GetDatabase("csharp_demo_db").GetCollection<BsonDocument>("products");
+
+            #region Find list collection
+            //BsonDocument doc = new BsonDocument();
+            //List<BsonDocument> list_docs = collection.Find(doc).ToList();
+            //foreach (var item in list_docs)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region Find With query Collection
+            BsonDocument doc = new BsonDocument();
+            doc.Add("price", 180);
+            List<BsonDocument> list_docs = collection.Find(doc).ToList();
+            foreach (var item in list_docs)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
--- DemoMongo/Program.cs
+++ DemoMongo/Program.cs
@@ -1,17 +1,12 @@
-﻿ using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DemoMongo
+﻿namespace DemoMongo
 {
     class Program
     {
         static void Main(string[] args)
         {
             // DemoSet1.main();
-            InsertDemo.main();
+            // InsertDemo.main();
+            FindDemo.main();
         }
     }
 }
